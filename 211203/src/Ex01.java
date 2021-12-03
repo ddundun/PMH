00be320a591703dfadf18d93f665825d262ca0a6
ddundun <92000414+ddundun@users.noreply.github.com>
2021-12-03 09:48:50 +0900

    Add files via upload

---
+++ 211203/src/Ex01.java
@@ -0,0 +1,55 @@
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE}) // 클래스, 메서드 , 변수
+@Retention(RetentionPolicy.RUNTIME) //실행도중.. 소스로만..
+@interface MyAnnotation {
+    String a() default "aa";
+
+    int b() default 10;
+
+}
+
+@MyAnnotation(a = "Ex01", b = 400)
+public class Ex01 {
+    public Ex01() {
+        Field fields[] = this.getClass().getDeclaredFields(); //변수 가져오는 함수
+
+        for (Field f : fields) {
+            System.out.println(f);
+            Annotation ao = f.getAnnotation(MyAnnotation.class);
+            System.out.println(ao);
+            System.out.println();
+        }
+        Method methods[] = this.getClass().getDeclaredMethods(); //메서드 가져오는 함수
+
+        for (Method f : methods) {
+            System.out.println(f);
+            Annotation ao = f.getAnnotation(MyAnnotation.class);
+            System.out.println(ao);
+            System.out.println();
+        }
+
+        MyAnnotation[] ma = this.getClass().getAnnotationsByType(MyAnnotation.class);  //ex01클래스에 붙은 어노테이션 가져오는 함수
+//        Annotation ano = this.getClass().getAnnotation(MyAnnotation.class);
+        System.out.println(ma[0].a());
+        System.out.println(ma[0].b());
+    }
+
+    @MyAnnotation
+    String s1 = "f";
+
+
+    @MyAnnotation(a = "aaa", b = 20)
+    int s2 = 10;
+
+    @MyAnnotation(a = "doAmethod", b = 30)
+    public void doA() {
+    }
+
+    public static void main(String[] args) {
+        new Ex01();
+//d
+    }
+}
